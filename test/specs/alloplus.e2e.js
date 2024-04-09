3f47e252f2285b5c7de55027c26776cd758f1817
Shoroh1 <diy.syl.official@gmail.com>
2024-04-10 00:43:56 +0300

    update project

--- test/specs/alloplus.e2e.js
+++ test/specs/alloplus.e2e.js
@@ -1,4 +1,11 @@
-describe ('alloplus test', () => {
+describe ('alloplus.by test', () => {
+
+    it('should close cookies', async() => {
+        await browser.url('https://alloplus.by/');
+        const closeCookies = await $('.modal-popup > a').click();
+    });
+
+
     it('should error message to login page ', async () => {
         await browser.url('https://alloplus.by/');
         const lcButton = await $('.personal_wrap').click();
@@ -9,6 +16,15 @@ describe ('alloplus test', () => {
         expect(await loginError.getText()).toEqual('Неверный логин или пароль');
     });
 
-    it
+    it('should search input', async () => {
+        await browser.url('https://alloplus.by/');
+        const clickInputField = await $('.search-input#title-search-input_fixed[placeholder="Поиск"]').setValue('iPhone')
+        const searchButton = await $('.search-button-div [type="submit"]').click();
+        await browser.scroll(0, 600);
+        expect(await browser.getUrl()).toEqual('https://alloplus.by/catalog/?q=iPhone&s=%D0%9F%D0%BE%D0%B8%D1%81%D0%BA');
+        await browser.pause(5000);
+    });
+
+
 })
 
